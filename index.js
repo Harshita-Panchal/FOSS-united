0cf273aebaf70b7b0ca533aa043cac795b440c6b
Harshita-Panchal <107474403+Harshita-Panchal@users.noreply.github.com>
2022-11-16 16:08:48 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+/**
+ * @desc Toggle hamburger menu
+ */
 function myFunction() {
-  if(document.getElementById("hide").style.display =="none") {
-    document.getElementById("hide").style.display = "block";
+  if(document.getElementById("hide").style.display =="none") {          // display None
+    document.getElementById("hide").style.display = "block";            // display block
   }
   else {
-    document.getElementById("hide").style.display = "none"
+    document.getElementById("hide").style.display = "none"              // display None
   }
 }
 
-// ------Dropdown-------
+
+/**
+ * @desc Toggle dropdown mode
+ */
 let click = document.querySelector('.click');
  let list = document.querySelector('.list')
  click.addEventListener("click", () => {
@@ -15,6 +21,9 @@ let click = document.querySelector('.click');
  })
 
 
+ /**
+ * @desc Toggle dropdown mode
+ */
  let click2 = document.querySelector('.click2');
  let list2 = document.querySelector('.list2')
  click2.addEventListener("click", () => {
